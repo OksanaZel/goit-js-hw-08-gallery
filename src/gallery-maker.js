3679f94a6936a88e9f567f129717da94919ed37b
Oksana Zelenevskaya <exoticisl@gmail.com>
2021-04-06 23:15:34 +0300

    Создает и рендерит разметку

--- src/gallery-maker.js
+++ src/gallery-maker.js
@@ -1,6 +1,23 @@
 import galleryItems from "./gallery-items.js";
-console.log(galleryItems);
 
+/*Создание и рендер разметки по массиву данных и предоставленному шаблону. */
 const galleryList = document.querySelector('.gallery');
-console.log(galleryList);
 
+const createGalleryItem = ({ preview, original, description }) => {
+    return `<li class="gallery__item">
+  <a
+    class="gallery__link"
+    href="${original}"
+  >
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>`
+}
+
+const markup = galleryItems.map(createGalleryItem).join('');
+galleryList.insertAdjacentHTML("beforeend", markup);
