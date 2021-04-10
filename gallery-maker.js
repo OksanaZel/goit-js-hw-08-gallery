f74f654a8788357af4a9fc3e5fe49c0367c8e359
Oksana Zelenevskaya <exoticisl@gmail.com>
2021-04-10 21:43:04 +0300

    Добавляет решение задачи

--- gallery-maker.js
+++ gallery-maker.js
@@ -2,14 +2,12 @@ import galleryItems from "./gallery-items.js";
 
 /*Создание и рендер разметки по массиву данных и предоставленному шаблону. */
 const galleryList = document.querySelector('.gallery');
+// console.log(galleryList);
 
 const createGalleryItem = ({ preview, original, description }) => {
   return `<li class="gallery__item">
-    <a
-    class="gallery__link"
-    href="${original}"
-  >
-    <img
+  <a class="gallery__link" href="${original}">
+  <img
       class="gallery__image"
       src="${preview}"
       data-source="${original}"
@@ -24,89 +22,111 @@ galleryList.insertAdjacentHTML("beforeend", markup);
   
 /*Реализация делегирования на галерее ul.js-gallery и получение url большого изображения. */
 
-// galleryList.addEventListener('click', getOriginalImageUrl);
+galleryList.addEventListener('click', getOriginalImageUrl);
 
-// function getOriginalImageUrl(evt) {
-//   evt.preventDefault();
+function getOriginalImageUrl(evt) {
+  evt.preventDefault();
 
-//   if (evt.target.nodeName !== 'IMG') {
-//     return;
-//   }
+  if (evt.target.nodeName !== 'IMG') {
+    return;
+  }
 
-//   const currentImgUrl = evt.target.dataset.source;
-//   const currentImgAlt = evt.target.alt;
+  const currentImgUrl = evt.target.dataset.source;
+  const currentImgAlt = evt.target.alt;
 
-//   addCurrentImageAttr(currentImgUrl, currentImgAlt);
-//   openModal();
-// }
+  addCurrentImageAttr(currentImgUrl, currentImgAlt);
+  openModal();
+
+  const imagesListLink = galleryItems.map(item => item.original);
+  let currentIdx = imagesListLink.indexOf(currentImgUrl);
+  const imagesListDescription = galleryItems.map(item => item.description);
+  // let currentDescription = imagesListDescription.indexOf(currentImgAlt);
+  // console.log(currentDescription);
+  
+  window.addEventListener('keydown', turnaboutImg);
+  
+  const currentImage = document.querySelector('.lightbox__image');
+  // console.log(currentImage);
+
+  function turnaboutImg(evt) {
+
+      if (evt.code === 'ArrowRight' && modalIsOpen.classList.contains('is-open')) {
+      currentIdx += 1;
+        currentImage.src = imagesListLink[currentIdx];
+        currentImage.alt = imagesListDescription[currentIdx];
+    }
+
+    if (evt.code === 'ArrowLeft' && modalIsOpen.classList.contains('is-open')) {
+      currentIdx -= 1;
+      currentImage.src = imagesListLink[currentIdx];
+      currentImage.alt = imagesListDescription[currentIdx];
+    }
+    }
+}
 
 /*Открытие модального окна по клику на элементе галереи. */
 
-// const modalIsOpen = document.querySelector('.lightbox');
+const modalIsOpen = document.querySelector('.lightbox');
 
-// function openModal() {
-//   modalIsOpen.classList.add('is-open');
-// }
+function openModal() {
+  modalIsOpen.classList.add('is-open');
+}
 
 /*Подмена значения атрибута src элемента img.lightbox__image. */
 
-// const originalImageAttr = document.querySelector('.lightbox__image');
+const originalImageAttr = document.querySelector('.lightbox__image');
 
-// function addCurrentImageAttr (url, alt){
-//   originalImageAttr.src = `${url}`;
-//   originalImageAttr.alt = `${alt}`;
-// }
-
-// window.addEventListener('keydown', turnaboutImg);
-
-//   function turnaboutImg (evt){
-//   if (evt.code === 'ArrowRight' && modalIsOpen.classList.contains('is-open')) {
-//     console.log(evt.code);
-//   }
-
-//   if(evt.code === 'ArrowLeft' && modalIsOpen.classList.contains('is-open')) {
-//     console.log(evt.code);
-//   }
-// }
+function addCurrentImageAttr (url, alt){
+  originalImageAttr.src = `${url}`;
+  originalImageAttr.alt = `${alt}`;
+}
 
 /*Закрытие модального окна по клику на кнопку button[data-action="close-lightbox"]. */
 /*Закрытие модального окна по клику на div.lightbox__overlay.
 Закрытие модального окна по нажатию клавиши ESC. */
 
-// const modalIsClose = document.querySelector('button[data-action="close-lightbox"]');
-// modalIsClose.addEventListener('click', closeModal);
+const modalIsClose = document.querySelector('button[data-action="close-lightbox"]');
+modalIsClose.addEventListener('click', closeModal);
 
-// const modalOverlay = document.querySelector('.lightbox__overlay');
-// modalOverlay.addEventListener('click', closeModal);
+const modalOverlay = document.querySelector('.lightbox__overlay');
+modalOverlay.addEventListener('click', closeModal);
 
-// function closeModal() {
-//   modalIsOpen.classList.remove('is-open');
-//   clearImageSrc();
-// }
+function closeModal() {
+  modalIsOpen.classList.remove('is-open');
+  clearImageSrc();
+}
 
-// window.addEventListener('keydown', onEscPress);
+window.addEventListener('keydown', onEscPress);
 
-// function onEscPress(evt) {
-//   if (evt.code === 'Escape') {
-//     closeModal();
-//   }
-// }
+function onEscPress(evt) {
+  if (evt.code === 'Escape') {
+    closeModal();
+  }
+}
 
 /*Очистка значения атрибута src элемента img.lightbox__image. Это необходимо для того, 
 чтобы при следующем открытии модального окна, пока грузится изображение, мы не видели предыдущее. */
 
-// function clearImageSrc() {
-//   originalImageAttr.src = '';
-//   originalImageAttr.alt = '';
-// }
+function clearImageSrc() {
+  originalImageAttr.src = '';
+  originalImageAttr.alt = '';
+}
 
 /*Пролистывание изображений галереи в открытом модальном окне клавишами "влево" и "вправо". */
-const galleryListItem = galleryList.querySelector('.gallery__item');
-console.log(galleryListItem);
-// const galleryItem = galleryListItems.forEach((item, index) => console.log(index));
+// const galleryListItems = document.querySelector('.gallery__item');
+// console.log(galleryListItems);
 
-window.addEventListener('keydown', (evt) => {
-  if (evt.code === 'ArrowRight') {
-    console.log(evt.code);
-  }
-})
+// window.addEventListener('keydown', changeSibling);
+
+//   function changeSibling(evt){
+//   if (evt.code === 'ArrowRight') {
+//     console.log(galleryListItems.nextSibling.childNodes);
+//   }
+
+//   if (evt.code === 'ArrowLeft') {
+//     console.log(galleryListItems.previousSibling);
+//   }
+// }
+
+// const imagesList = galleryItems.map(item => item.original);
+// console.log(imagesList);
