0cb78dd6e7c35214d2a845e2d7829e213b47a683
Oksana Zelenevskaya <exoticisl@gmail.com>
2021-04-16 00:05:51 +0300

    Добавляет решение задачи

--- gallery-maker.js
+++ gallery-maker.js
@@ -5,23 +5,13 @@ const refs = {
   modalIsOpen: document.querySelector('.lightbox'),
   originalImageAttr: document.querySelector('.lightbox__image'),
   modalIsClose: document.querySelector('button[data-action="close-lightbox"]'),
-  modalOverlay: document.querySelector('.lightbox__overlay'),
-  currentImage: document.querySelector('.lightbox__image'),
 }
 
 /*Создание и рендер разметки по массиву данных и предоставленному шаблону. */
 
 const createGalleryItem = ({ preview, original, description }) => {
-  return `<li class="gallery__item"><a class="gallery__link" href="${original}">
-  <img
-      class="gallery__image"
-      src="${preview}"
-      data-source="${original}"
-      alt="${description}"
-    />
-    </a>
-</li>`
-}
+  return `<li class="gallery__item"><a class="gallery__link" href="${original}"><img class="gallery__image" src="${preview}"
+      data-source="${original}" alt="${description}"/></a></li>`}
 
 const markup = galleryItems.map(createGalleryItem).join('');
 refs.galleryList.insertAdjacentHTML("beforeend", markup);
@@ -39,38 +29,44 @@ function onImageClick(evt) {
 
   openModal();
 
-  const currentImgUrl = evt.target.dataset.source;
-  const currentImgAlt = evt.target.alt;
+/*Подмена значения атрибута src элемента img.lightbox__image. */
+  refs.originalImageAttr.src = evt.target.dataset.source;
+  refs.originalImageAttr.alt = evt.target.alt;
+  console.log(refs.originalImageAttr.src);
+  console.log(refs.originalImageAttr.alt)
 
-  addCurrentImageAttr(currentImgUrl, currentImgAlt);
+
+  
+
+  // addCurrentImageAttr(currentImgUrl, currentImgAlt);
 
   /*Пролистывание изображений галереи в открытом модальном окне клавишами "влево" и "вправо". */
-  const imagesListLink = galleryItems.map(item => item.original);
-  const imagesListDescription = galleryItems.map(item => item.description);
-  let currentIdx = imagesListLink.indexOf(currentImgUrl);
+  // const imagesListLink = galleryItems.map(item => item.original);
+  // const imagesListDescription = galleryItems.map(item => item.description);
+  // let currentIdx = imagesListLink.indexOf(currentImgUrl);
   
-  window.addEventListener('keydown', changeImage);
+  // window.addEventListener('keydown', changeImage);
 
-  function changeImage(evt) {
+  // function changeImage(evt) {
 
-    if (evt.code === 'ArrowRight') {
-      if (currentIdx >= galleryItems.length - 1) {
-        return;
-      }
-        currentIdx += 1;
-        refs.currentImage.src = imagesListLink[currentIdx];
-        refs.currentImage.alt = imagesListDescription[currentIdx];
-    }
+  //   if (evt.code === 'ArrowRight') {
+  //     if (currentIdx >= galleryItems.length - 1) {
+  //       return;
+  //     }
+  //       currentIdx += 1;
+  //       refs.currentImage.src = imagesListLink[currentIdx];
+  //       refs.currentImage.alt = imagesListDescription[currentIdx];
+  //   }
 
-    if (evt.code === 'ArrowLeft') {
-      if (currentIdx <= 0) {
-        return;
-      }
-        currentIdx -= 1;
-        refs.currentImage.src = imagesListLink[currentIdx];
-        refs.currentImage.alt = imagesListDescription[currentIdx];
-    }
-  }
+  //   if (evt.code === 'ArrowLeft') {
+  //     if (currentIdx <= 0) {
+  //       return;
+  //     }
+  //       currentIdx -= 1;
+  //       refs.currentImage.src = imagesListLink[currentIdx];
+  //       refs.currentImage.alt = imagesListDescription[currentIdx];
+  //   }
+  // }
 }
 
 /*Открытие модального окна по клику на элементе галереи. */
@@ -82,22 +78,33 @@ function openModal() {
 
 /*Подмена значения атрибута src элемента img.lightbox__image. */
 
-function addCurrentImageAttr (url, alt){
-  refs.originalImageAttr.src = `${url}`;
-  refs.originalImageAttr.alt = `${alt}`;
-}
+// function addCurrentImageAttr (url, alt){
+//   refs.originalImageAttr.src = `${url}`;
+//   refs.originalImageAttr.alt = `${alt}`;
+// }
 
 /*Закрытие модального окна по клику на кнопку button[data-action="close-lightbox"]. */
 /*Закрытие модального окна по клику на div.lightbox__overlay.
 Закрытие модального окна по нажатию клавиши ESC. */
 
 refs.modalIsClose.addEventListener('click', closeModal);
-refs.modalOverlay.addEventListener('click', closeModal);
 
 function closeModal() {
   window.removeEventListener('keydown', onEscPress);
   refs.modalIsOpen.classList.remove('is-open');
-  clearImageSrc();
+
+ /*Очистка значения атрибута src элемента img.lightbox__image. Это необходимо для того,
+чтобы при следующем открытии модального окна, пока грузится изображение, мы не видели предыдущее. */
+  refs.originalImageAttr.src = '';
+  refs.originalImageAttr.alt = '';
+}
+
+refs.modalIsOpen.addEventListener('click', onOverlayClickClose);
+
+function onOverlayClickClose (evt) {
+  if (evt.target.nodeName !== 'IMG') {
+    closeModal();
+  }
 }
 
 function onEscPress(evt) {
@@ -109,7 +116,10 @@ function onEscPress(evt) {
 /*Очистка значения атрибута src элемента img.lightbox__image. Это необходимо для того, 
 чтобы при следующем открытии модального окна, пока грузится изображение, мы не видели предыдущее. */
 
-function clearImageSrc() {
-  refs.originalImageAttr.src = '';
-  refs. originalImageAttr.alt = '';
-}
+// function clearImageSrc() {
+//   refs.originalImageAttr.src = '';
+//   refs. originalImageAttr.alt = '';
+// }
+
+/*Пролистывание изображений галереи в открытом модальном окне клавишами "влево" и "вправо". */
+  
