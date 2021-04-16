1145e14d61c34bad422c3525ea7de5e19438ed9e
Oksana Zelenevskaya <exoticisl@gmail.com>
2021-04-16 14:41:40 +0300

    Добавляет решение

--- gallery-maker.js
+++ gallery-maker.js
@@ -7,14 +7,17 @@ const refs = {
   modalIsClose: document.querySelector('button[data-action="close-lightbox"]'),
 }
 
+let currentIndex;
+
 /*Создание и рендер разметки по массиву данных и предоставленному шаблону. */
 
 const createGalleryItem = ({ preview, original, description }) => {
   return `<li class="gallery__item"><a class="gallery__link" href="${original}"><img class="gallery__image" src="${preview}"
       data-source="${original}" alt="${description}"/></a></li>`}
 
-const markup = galleryItems.map(createGalleryItem).join('');
-refs.galleryList.insertAdjacentHTML("beforeend", markup);
+const markup = galleryItems.map(createGalleryItem);
+refs.galleryList.insertAdjacentHTML("beforeend", markup.join(''));
+// console.log(markup);
   
 /*Реализация делегирования на галерее ul.js-gallery и получение url большого изображения. */
 
@@ -29,42 +32,51 @@ function onImageClick(evt) {
 
   openModal();
 
-/*Подмена значения атрибута src элемента img.lightbox__image. */
+  /*Подмена значения атрибута src элемента img.lightbox__image. */
   refs.originalImageAttr.src = evt.target.dataset.source;
   refs.originalImageAttr.alt = evt.target.alt;
-  console.log(refs.originalImageAttr.src);
-  console.log(refs.originalImageAttr.alt)
+
+  markup.forEach((item, index) => {
+    if (item.includes(evt.target.dataset.source)) {
+      currentIndex = index;
+    }
+  })
+
+  // window.addEventListener('keydown', changeImage);
+
+  // changeImage();
+}
 
   // addCurrentImageAttr(currentImgUrl, currentImgAlt);
 
   /*Пролистывание изображений галереи в открытом модальном окне клавишами "влево" и "вправо". */
   // const imagesListLink = galleryItems.map(item => item.original);
   // const imagesListDescription = galleryItems.map(item => item.description);
-  // let currentIdx = imagesListLink.indexOf(currentImgUrl);
+  // let currentIdx = markup.indexOf(refs.originalImageAttr.src);
+  // console.log(currentIdx);
   
-  // window.addEventListener('keydown', changeImage);
+  window.addEventListener('keydown', changeImage);
 
-  // function changeImage(evt) {
+  function changeImage(evt) {
 
-  //   if (evt.code === 'ArrowRight') {
-  //     if (currentIdx >= galleryItems.length - 1) {
-  //       return;
-  //     }
-  //       currentIdx += 1;
-  //       refs.currentImage.src = imagesListLink[currentIdx];
-  //       refs.currentImage.alt = imagesListDescription[currentIdx];
-  //   }
+    if (evt.code === 'ArrowRight') {
+      if (currentIndex >= galleryList.length - 1) {
+        return;
+      }
+        currentIndex += 1;
+        refs.originalImageAttr.src = galleryList[currentIndex];
+        refs.originalImageAttr.alt = galleryList[currentIndex];
+    }
 
-  //   if (evt.code === 'ArrowLeft') {
-  //     if (currentIdx <= 0) {
-  //       return;
-  //     }
-  //       currentIdx -= 1;
-  //       refs.currentImage.src = imagesListLink[currentIdx];
-  //       refs.currentImage.alt = imagesListDescription[currentIdx];
-  //   }
-  // }
-}
+    if (evt.code === 'ArrowLeft') {
+      if (currentIndex <= 0) {
+        return;
+      }
+        currentIndex -= 1;
+        refs.originalImageAttr.src = galleryList[currentIndex];
+        refs.originalImageAttr.alt = galleryList[currentIndex];
+    }
+  }
 
 /*Открытие модального окна по клику на элементе галереи. */
 
@@ -98,8 +110,6 @@ function closeModal() {
   refs.originalImageAttr.alt = '';
 }
 
-// refs.modalIsOpen.addEventListener('click', onOverlayClickClose);
-
 function onOverlayClickClose (evt) {
   if (evt.target.nodeName !== 'IMG') {
     closeModal();
@@ -121,4 +131,8 @@ function onEscPress(evt) {
 // }
 
 /*Пролистывание изображений галереи в открытом модальном окне клавишами "влево" и "вправо". */
+// const imagesListLink = galleryItems.map(item => item.original);
+// const imagesListDescription = galleryItems.map(item => item.description);
+// console.log(imagesListLink);
+// console.log(imagesListDescription);
   
