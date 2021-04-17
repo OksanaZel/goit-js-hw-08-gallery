4586ac0b45da58532a1c26f9daa49c3d4efb2f13
Oksana Zelenevskaya <exoticisl@gmail.com>
2021-04-17 16:01:48 +0300

    Добавляет решение

--- gallery-maker.js
+++ gallery-maker.js
@@ -7,17 +7,14 @@ const refs = {
   modalIsClose: document.querySelector('button[data-action="close-lightbox"]'),
 }
 
-let currentIndex;
-
 /*Создание и рендер разметки по массиву данных и предоставленному шаблону. */
 
 const createGalleryItem = ({ preview, original, description }) => {
   return `<li class="gallery__item"><a class="gallery__link" href="${original}"><img class="gallery__image" src="${preview}"
       data-source="${original}" alt="${description}"/></a></li>`}
 
-const markup = galleryItems.map(createGalleryItem);
-refs.galleryList.insertAdjacentHTML("beforeend", markup.join(''));
-// console.log(markup);
+const markup = galleryItems.map(createGalleryItem).join('');
+refs.galleryList.insertAdjacentHTML("beforeend", markup);
   
 /*Реализация делегирования на галерее ul.js-gallery и получение url большого изображения. */
 
@@ -36,47 +33,9 @@ function onImageClick(evt) {
   refs.originalImageAttr.src = evt.target.dataset.source;
   refs.originalImageAttr.alt = evt.target.alt;
 
-  markup.forEach((item, index) => {
-    if (item.includes(evt.target.dataset.source)) {
-      currentIndex = index;
-    }
-  })
+  changeImage();
 
-  // window.addEventListener('keydown', changeImage);
-
-  // changeImage();
 }
-
-  // addCurrentImageAttr(currentImgUrl, currentImgAlt);
-
-  /*Пролистывание изображений галереи в открытом модальном окне клавишами "влево" и "вправо". */
-  // const imagesListLink = galleryItems.map(item => item.original);
-  // const imagesListDescription = galleryItems.map(item => item.description);
-  // let currentIdx = markup.indexOf(refs.originalImageAttr.src);
-  // console.log(currentIdx);
-  
-  window.addEventListener('keydown', changeImage);
-
-  function changeImage(evt) {
-
-    if (evt.code === 'ArrowRight') {
-      if (currentIndex >= galleryList.length - 1) {
-        return;
-      }
-        currentIndex += 1;
-        refs.originalImageAttr.src = galleryList[currentIndex];
-        refs.originalImageAttr.alt = galleryList[currentIndex];
-    }
-
-    if (evt.code === 'ArrowLeft') {
-      if (currentIndex <= 0) {
-        return;
-      }
-        currentIndex -= 1;
-        refs.originalImageAttr.src = galleryList[currentIndex];
-        refs.originalImageAttr.alt = galleryList[currentIndex];
-    }
-  }
 
 /*Открытие модального окна по клику на элементе галереи. */
 
@@ -84,14 +43,9 @@ function openModal() {
   refs.modalIsOpen.classList.add('is-open');
   window.addEventListener('keydown', onEscPress);
   refs.modalIsOpen.addEventListener('click', onOverlayClickClose);
+  window.addEventListener('keydown', changeImage);
 }
 
-/*Подмена значения атрибута src элемента img.lightbox__image. */
-
-// function addCurrentImageAttr (url, alt){
-//   refs.originalImageAttr.src = `${url}`;
-//   refs.originalImageAttr.alt = `${alt}`;
-// }
 
 /*Закрытие модального окна по клику на кнопку button[data-action="close-lightbox"]. */
 /*Закрытие модального окна по клику на div.lightbox__overlay.
@@ -101,7 +55,8 @@ refs.modalIsClose.addEventListener('click', closeModal);
 
 function closeModal() {
   window.removeEventListener('keydown', onEscPress);
-  refs.modalIsOpen.removeEventListener('click', onOverlayClickClose);
+  refs.modalIsOpen.removeEventListener('click', onOverlayClickClose)
+  window.removeEventListener('keydown', changeImage);
   refs.modalIsOpen.classList.remove('is-open');
 
  /*Очистка значения атрибута src элемента img.lightbox__image. Это необходимо для того,
@@ -122,17 +77,29 @@ function onEscPress(evt) {
   }
 }
 
-/*Очистка значения атрибута src элемента img.lightbox__image. Это необходимо для того, 
-чтобы при следующем открытии модального окна, пока грузится изображение, мы не видели предыдущее. */
+ /*Пролистывание изображений галереи в открытом модальном окне клавишами "влево" и "вправо". */
+const imagesListLink = galleryItems.map(item => item.original);
+const imagesListDescription = galleryItems.map(item => item.description);
+let currentIdx = imagesListLink.indexOf(refs.originalImageAttr.src);
+console.log(currentIdx);
 
-// function clearImageSrc() {
-//   refs.originalImageAttr.src = '';
-//   refs. originalImageAttr.alt = '';
-// }
+  function changeImage(evt) {
 
-/*Пролистывание изображений галереи в открытом модальном окне клавишами "влево" и "вправо". */
-// const imagesListLink = galleryItems.map(item => item.original);
-// const imagesListDescription = galleryItems.map(item => item.description);
-// console.log(imagesListLink);
-// console.log(imagesListDescription);
-  
+    if (evt.code === 'ArrowRight') {
+      if (currentIdx >= imagesListLink.length - 1) {
+        return;
+      }
+      currentIdx += 1;
+      refs.originalImageAttr.src = imagesListLink[currentIdx];
+      refs.originalImageAttr.alt = imagesListLink[currentIdx];
+    }
+
+    if (evt.code === 'ArrowLeft') {
+      if (currentIdx <= 0) {
+        return;
+      }
+      currentIdx -= 1;
+      refs.originalImageAttr.src = imagesListLink[currentIdx];
+      refs.originalImageAttr.alt = imagesListLink[currentIdx];
+    }
+  }
